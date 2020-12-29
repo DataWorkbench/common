cdd43a0bb860377c6c9b302acf41f93b111568a7
zhang lian zhuang <zlianzhuang@163.com>
2020-12-29 15:52:28 +0800

    add source url type
    mysql/postgresql/kafka connection string

--- constants/worksource.go
+++ constants/worksource.go
@@ -6,7 +6,36 @@ const (
 	SourceTypePostgreSQL = "PostgreSQL"
 	SourceTypeKafka      = "Kafka"
 	TableTypeDimension   = "d"
-	TableTypeComment     = "c"
+	TableTypeCommon      = "c"
 	CreatorWorkBench     = "workbench" //can't drop by custom,  workbench is auto create when spark/other engine created
 	CreatorCustom        = "custom"
 )
+
+type SourceMysqlParams struct {
+	User             string   `json:"user"`
+	Password         string   `json:"password"`
+	Host             string   `json:"host"`
+	Port             int32    `json:"port"`
+	Database         string   `json:"database"`
+	ConnectorOptions []string `json:"connector_options"`
+}
+
+type SourcePostgreSQLParams struct {
+	User             string   `json:"user"`
+	Password         string   `json:"password"`
+	Host             string   `json:"host"`
+	Port             int32    `json:"port"`
+	Database         string   `json:"database"`
+	ConnectorOptions []string `json:"connector_options"`
+}
+
+type SourceKafkaParams struct {
+	Host             string   `json:"host"`
+	Port             int32    `json:"port"`
+	ConnectorOptions []string `json:"connector_options"`
+}
+
+type FlinkTableDefine struct {
+	SqlColumn        []string `json:"sql_column"`
+	ConnectorOptions []string `json:"connector_options"`
+}
