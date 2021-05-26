e2d258b93bee3e8f9853530138c6ad95c81fb263
wuyu <wuyu@yunify.com>
2021-05-27 00:58:43 +0800

    constants: Add const OpType

--- constants/workspace.go
+++ constants/workspace.go
@@ -11,10 +11,20 @@ const (
 	RoleTypeSystem int32 = iota + 1
 	RoleTypeCustom
 )
+
+// Workspace system role id.
 const (
 	RoleIdSpaceOwner     = "rs-1001"
 	RoleIdSpaceAdmin     = "rs-1002"
 	RoleIdSpaceDeveloper = "rs-1003"
 	RoleIdSpaceOperator  = "rs-1004"
 	RoleIdSpaceVisitor   = "rs-1005"
+)
+
+// Operation type.
+const (
+	OpTypeUnknown int32 = iota + 1
+	OpTypeRead
+	OpTypeWrite
+	OpTypeDelete
 )
