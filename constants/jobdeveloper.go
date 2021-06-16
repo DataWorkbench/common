7fa8704914b5eaf99e7ba56c6a0ee81240c080c8
zhang lian zhuang <zlianzhuang@163.com>
2021-06-16 14:44:28 +0800

    remove duplicate type JSONString

--- constants/jobdeveloper.go
+++ constants/jobdeveloper.go
@@ -194,17 +194,3 @@ type FlinkParagraphsInfo struct {
 type JobFreeActionFlink struct {
 	ZeppelinDeleteJar string `json:"zeppelindeletejar"`
 }
-
-type JSONString string
-
-func (s *JSONString) UnmarshalJSON(b []byte) error {
-	*s = JSONString(b)
-	return nil
-}
-
-func (s JSONString) MarshalJSON() ([]byte, error) {
-	if s == "" {
-		return []byte("{}"), nil
-	}
-	return []byte(s), nil
-}
