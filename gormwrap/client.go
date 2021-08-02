aedfb21b7dd7c8ac24f85b5125c509e88bcaff11
zhang lian zhuang <zlianzhuang@163.com>
2021-08-03 01:04:45 +0800

    mysql don't use transaction.

--- gormwrap/client.go
+++ gormwrap/client.go
@@ -56,6 +56,7 @@ func NewMySQLConn(ctx context.Context, cfg *MySQLConfig, options ...Option) (db 
 	)
 
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
 		Logger: &Logger{
 			Level:         LogLevel(cfg.LogLevel),
 			SlowThreshold: cfg.SlowThreshold,
