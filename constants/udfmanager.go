bce98bfe4cbed0d622669333b5b0b6d1978cc777
zhang lian zhuang <zlianzhuang@163.com>
2021-08-04 10:21:05 +0800

    udfmanager
    addition python udf.

--- constants/udfmanager.go
+++ constants/udfmanager.go
@@ -1,9 +1,12 @@
 package constants
 
 const (
-	UdfTypeScala    = "Scala"
-	UdfTypePython   = "Python"
-	UdfTypeJarUDF   = "JarUDF"
-	UdfTypeJarUDTF  = "JarUDTF"
-	UdfTypeJarUDTTF = "JarUDTTF"
+	ScalaUDF   = "ScalaUDF"
+	ScalaUDTF  = "ScalaUDTF"
+	ScalaUDTTF = "ScalaUDTTF" //https://ci.apache.org/projects/flink/flink-docs-release-1.11/dev/table/streaming/temporal_tables.html#defining-temporal-table-function
+	JarUDF     = "JarUDF"
+	JarUDTF    = "JarUDTF"
+	JarUDTTF   = "JarUDTTF"
+	PythonUDF  = "PythonUDF"
+	PythonUDTF = "PythonUDTF"
 )
