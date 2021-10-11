b14e7bbb61e2c92f48ca9ae6457c51756eeb190f
zhang lian zhuang <zlianzhuang@163.com>
2021-10-12 03:23:48 +0800

    update udfmanager

--- constants/udfmanager.go
+++ constants/udfmanager.go
@@ -1,13 +1,10 @@
 package constants
 
 const (
-	ScalaUDF   = "ScalaUDF"
-	ScalaUDTF  = "ScalaUDTF"
-	ScalaUDTTF = "ScalaUDTTF" //https://ci.apache.org/projects/flink/flink-docs-release-1.11/dev/table/streaming/temporal_tables.html#defining-temporal-table-function
-	JarUDF     = "JarUDF"
-	JarUDTF    = "JarUDTF"
-	JarUDTTF   = "JarUDTTF"
-	PythonUDF  = "PythonUDF"
-	PythonUDTF = "PythonUDTF"
-	Dir        = "Dir"
+	UDFType           = "UDF"
+	UDTFType          = "UDTF"
+	UDTTFType         = "UDTTF" //https://ci.apache.org/projects/flink/flink-docs-release-1.11/dev/table/streaming/temporal_tables.html#defining-temporal-table-function
+	UDFLanguageScala  = "Scala"
+	UDFLanguageJava   = "Java"
+	UDFLanguagePython = "Python"
 )
