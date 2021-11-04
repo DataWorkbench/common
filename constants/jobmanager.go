56330b5baeb90798ae73a7bff8a18e71878bb8a2
zhang lian zhuang <zlianzhuang@163.com>
2021-11-04 09:22:18 +0800

    update jobdeveloper

--- constants/jobmanager.go
+++ constants/jobmanager.go
@@ -1,25 +1,12 @@
 package constants
 
-import "github.com/DataWorkbench/gproto/pkg/model"
-
 const (
-	StatusFailed           = model.StreamJobInst_Failed
-	StatusFailedString     = "failed"
-	StatusFinish           = model.StreamJobInst_Succeed
-	StatusFinishString     = "finish"
-	StatusRunning          = model.StreamJobInst_Running
-	StatusRunningString    = "running"
-	StatusTerminated       = model.StreamJobInst_Terminated
-	StatusTerminatedString = "terminated"
-
 	MessageUnknowState = "unknow state"
 	MessageFinish      = "finish and success"
 	MessageRunning     = "job running"
 	MessageFailed      = "error happend"
-	MessageTerminated  = "user terminated"
 
 	JobCommandRun     = "run"
 	JobCommandSyntax  = "syntax"
 	JobCommandPreview = "preview"
-	JobCommandExplain = "explain"
 )
