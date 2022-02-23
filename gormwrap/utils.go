f5f0161ec03073484533f135490473ee3f13e3cf
wuyu <wuyu@yunify.com>
2022-02-23 13:40:17 +0800

    gormwrap: Add utils func BuildConditionClauseInWithString

--- gormwrap/utils.go
+++ gormwrap/utils.go
@@ -6,20 +6,21 @@ import (
 
 // BuildClauseExpressionEQWithOR used to construct the conditions that the specified column
 // equal to any value in "items".
+// Deprecated: use gormwrap.BuildConditionClauseInWithString instead.
 func BuildClauseExpressionEQWithOR(column string, items []string) clause.Expression {
 	if len(items) == 0 {
-		panic("gorm BuildClauseExpressionEQWithOR: items cannot be empty")
+		panic("gormwrap BuildClauseExpressionEQWithOR: items cannot be empty")
 	}
 
 	if column == "" {
-		panic("gorm BuildClauseExpressionEQWithOR: column cannot be empty")
+		panic("gormwrap BuildClauseExpressionEQWithOR: column cannot be empty")
 	}
 
 	eqExpr := make([]clause.Expression, len(items))
 	for i := 0; i < len(items); i++ {
 		value := items[i]
 		if value == "" {
-			panic("gorm BuildClauseExpressionEQWithOR: the item value cannot be empty")
+			panic("gormwrap BuildClauseExpressionEQWithOR: the item value cannot be empty")
 		}
 		eqExpr[i] = clause.Eq{Column: column, Value: value}
 	}
@@ -30,4 +31,21 @@ func BuildClauseExpressionEQWithOR(column string, items []string) clause.Express
 		expr = clause.Or(eqExpr...)
 	}
 	return expr
+}
+
+// BuildConditionClauseInWithString used build the conditions of `Where IN` with string slice.
+func BuildConditionClauseInWithString(column string, items []string) clause.IN {
+	if len(items) == 0 {
+		panic("gormwrap BuildConditionClauseInWithString: items cannot be empty")
+	}
+
+	if column == "" {
+		panic("gormwrap BuildConditionClauseInWithString: column cannot be empty")
+	}
+
+	values := make([]interface{}, len(items))
+	for i := 0; i < len(items); i++ {
+		values[i] = items[i]
+	}
+	return clause.IN{Column: column, Values: values}
 }
