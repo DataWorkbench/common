7c8468ca07b9953419ee9125f8553760dc0f1fd5
jackyu <jackyu@yunify.com>
2022-04-06 00:46:09 +0800

    add k8s types

--- utils/k8s/helm/chartmeta.go
+++ utils/k8s/helm/chartmeta.go
@@ -8,49 +8,6 @@ import (
 const DefaultTimeoutSecond = 600
 const InstanceLabelKey = "app.kubernetes.io/instance"
 
-// firstAtAll, create docker registry secret by kubectl:
-// ***************************************************************
-// ImageConfig
-// ***************************************************************
-// kubectl create secret docker-registry my-docker-registry-secret
-//                                       --docker-server=<your-registry-server>
-//                                       --docker-username=<your-name>
-//                                       --docker-password=<your-pword>
-//                                       --docker-email=<your-email>
-type Image struct {
-	// TODO: check pull secrets
-	Registry    string   `json:"registry,omitempty" yaml:"registry,omitempty"`
-	PullSecrets []string `json:"pullSecrets,omitempty" yaml:"pullSecrets,omitempty"`
-	PullPolicy  string   `json:"pullPolicy,omitempty" yaml:"pullPolicy,omitempty"`
-
-	Tag string `json:"tag,omitempty" yaml:"-"`
-}
-
-// update from other if the field is ""
-func (i *Image) Update(source *Image) {
-	if source == nil {
-		return
-	}
-	if i.Registry == "" && source.Registry != "" {
-		i.Registry = source.Registry
-	}
-	if len(i.PullSecrets) == 0 && len(source.PullSecrets) > 0 {
-		i.PullSecrets = source.PullSecrets
-	}
-	if i.PullPolicy == "" && source.PullPolicy != "" {
-		i.PullPolicy = source.PullPolicy
-	}
-}
-
-type Resource struct {
-	Cpu    float32 `json:"cpu,omitempty"    yaml:"cpu,omitempty"`
-	Memory string `json:"memory,omitempty" yaml:"memory,omitempty"`
-}
-
-type Resources struct {
-	Limits   Resource `json:"limits,omitempty"   yaml:"limits,omitempty"`
-	Requests Resource `json:"requests,omitempty" yaml:"requests,omitempty"`
-}
 
 // ***************************************************************
 // ValuesMeta
