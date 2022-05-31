b5092ea436f5e8fcd81f03098515208900571bec
jackyu <jackyu@yunify.com>
2022-05-31 14:15:04 +0800

    feat: read yaml file

---
+++ utils/io/file.go
@@ -0,0 +1,32 @@
+package io
+
+import (
+"gopkg.in/yaml.v3"
+"io/ioutil"
+"os"
+)
+
+func FileExist(file string) (bool, error) {
+	_, err := os.Stat(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func ReadYaml(file string) (map[string]interface{}, error) {
+	var err error
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	m := map[string]interface{}{}
+	if err = yaml.Unmarshal(bytes, &m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
