507ed129c29c27f329271b9057896c070c4a2b1b
jackyu <jackyu@yunify.com>
2022-05-31 14:13:55 +0800

    feat: string-map util

---
+++ utils/funcutil/map.go
@@ -0,0 +1,21 @@
+package funcutil
+
+// Map b overwrite map a
+func MergeMaps(a, b map[string]interface{}) map[string]interface{} {
+	out := make(map[string]interface{}, len(a))
+	for k, v := range a {
+		out[k] = v
+	}
+	for k, v := range b {
+		if v, ok := v.(map[string]interface{}); ok {
+			if bv, ok := out[k]; ok {
+				if bv, ok := bv.(map[string]interface{}); ok {
+					out[k] = MergeMaps(bv, v)
+					continue
+				}
+			}
+		}
+		out[k] = v
+	}
+	return out
+}
