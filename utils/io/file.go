7c26520e0f5994f789b9cabfd929b8ae81a92511
jackyu <jackyu@yunify.com>
2022-07-12 16:55:57 +0800

    update: os path exist

--- utils/io/file.go
+++ utils/io/file.go
@@ -3,19 +3,7 @@ package io
 import (
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
-	"os"
 )
-
-func FileExist(file string) (bool, error) {
-	_, err := os.Stat(file)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false, nil
-		}
-		return false, err
-	}
-	return true, nil
-}
 
 func ReadYaml(file string) (map[string]interface{}, error) {
 	var err error
